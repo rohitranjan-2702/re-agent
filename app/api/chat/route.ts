82da1a9264b26bb6335a8b39afedc35f4ded6f88
rohitranjan-2702 <rohitranjan2702@gmail.com>
2025-09-15 18:05:22 +0530

    ai-sdk

---
+++ app/api/chat/route.ts
@@ -0,0 +1,27 @@
+import { streamText, UIMessage, convertToModelMessages } from "ai";
+import { google } from "@ai-sdk/google";
+
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const {
+    messages,
+    model,
+  }: {
+    messages: UIMessage[];
+    model: string;
+  } = await req.json();
+
+  const result = streamText({
+    model: google("gemini-2.0-flash-001"),
+    messages: convertToModelMessages(messages),
+    system:
+      "You are a helpful assistant that can answer questions and help with tasks",
+  });
+
+  // send sources and reasoning back to the client
+  return result.toUIMessageStreamResponse({
+    sendSources: true,
+    sendReasoning: true,
+  });
+}
